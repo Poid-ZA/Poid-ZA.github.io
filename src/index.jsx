7e380b5f696c2db6ee0599bc60df2382e7ccd489
Poid-ZA <52122023+Poid-ZA@users.noreply.github.com>
2025-04-28 10:34:54 +0200

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from "https://cdn.skypack.dev/react@18.2.0";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@18.2.0";
+import App from "./components/App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
